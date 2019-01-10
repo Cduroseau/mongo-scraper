52dc63312ba6c629ec2adacb680cf8a8bb90da72
unknown <chrisduroseau@yahoo.com>
2019-01-10 18:30:17 -0500

    2nd version

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ var mongoose = require("mongoose");
 var path = require("path");
 var expressHbs = require("express-handlebars");
 
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/saved", function(req, res) {
+  res.render("saved");
+});
 app.engine("handlebars", expressHbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
